fe47236ce4f7fe49c97bd13a66f1b7eac487e4ad
Geoffroy Warin <code@geowarin.com>
2021-08-20 12:49:54 +0200

    fix script

--- .bin/system.sh
+++ .bin/system.sh
@@ -1,9 +1,13 @@
 #! /bin/bash
 
 # Enable zram
-sed -i '/GRUB_CMDLINE_LINUX_DEFAULT=/ s/.$/ systemd.zram=1"/;s/" /"/' /etc/default/grub
+if ! (grep 'systemd.zram' /etc/default/grub); then
+    sed -i '/GRUB_CMDLINE_LINUX_DEFAULT=/ s/.$/ systemd.zram=1"/;s/" /"/' /etc/default/grub
+fi
 
-echo 'GRUB_DISABLE_OS_PROBER=false' | tee -a /etc/default/grub
+if ! (grep 'GRUB_DISABLE_OS_PROBER' /etc/default/grub); then
+    echo 'GRUB_DISABLE_OS_PROBER=false' | tee -a /etc/default/grub
+fi
 
 tee /etc/xdg/reflector/reflector.conf <<EOF
 --save /etc/pacman.d/mirrorlist
@@ -13,11 +17,11 @@ tee /etc/xdg/reflector/reflector.conf <<EOF
 --age 12
 EOF
 
-sed -i 's/^#Color$/Color/' /mnt/etc/pacman.conf
-sed -i 's/^#ParallelDownloads/ParallelDownloads/' /mnt/etc/pacman.conf
-sed -i 's/^#CheckSpace$/CheckSpace/' /mnt/etc/pacman.conf
-sed -i 's/^#VerbosePkgLists$/VerbosePkgLists/' /mnt/etc/pacman.conf
-sed -i '/^VerbosePkgLists$/ a ILoveCandy' /mnt/etc/pacman.conf
+sed -i 's/^#Color$/Color/' /etc/pacman.conf
+sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
+sed -i 's/^#CheckSpace$/CheckSpace/' /etc/pacman.conf
+sed -i 's/^#VerbosePkgLists$/VerbosePkgLists/' /etc/pacman.conf
+sed -i '/^VerbosePkgLists$/ a ILoveCandy' /etc/pacman.conf
 sed -i '/\[multilib]$/ {
     N
-    /Include/s/#//g}' /mnt/etc/pacman.conf
+    /Include/s/#//g}' /etc/pacman.conf
