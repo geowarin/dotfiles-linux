95ee108f9454a29dabec5938ef617e5945d9ebb0
Geoffroy Warin <code@geowarin.com>
2022-02-02 09:15:31 +0100

    starship, icons, smart gaps

--- .config/zsh/plugins.zsh
+++ .config/zsh/plugins.zsh
@@ -15,8 +15,5 @@ zplug "plugins/command-not-found", from:oh-my-zsh
 # Press esc-esc to add sudo to the previous command
 zplug "hcgraf/zsh-sudo"
 
-# Prompt
-zplug "spaceship-prompt/spaceship-prompt", use:spaceship.zsh, from:github, as:theme
-
 # Syntax highlighting needs to be loaded after compinit
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
