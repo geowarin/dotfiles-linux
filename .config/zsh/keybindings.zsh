09f17ba36b40fc28cf44894b1d1ae16d1ffee9e5
Geoffroy Warin <code@geowarin.com>
2022-03-13 19:34:45 +0100

    zsh: fix keybindings

--- .config/zsh/keybindings.zsh
+++ .config/zsh/keybindings.zsh
@@ -4,34 +4,56 @@ bindkey -e
 # add / to wordchars for ctrl+left/right
 WORDCHARS=$WORDCHARS:s:/:
 
-# [PageUp] - Up a line of history
-bindkey -M emacs "${terminfo[kpp]}" up-line-or-history
-# [PageDown] - Down a line of history
-bindkey -M emacs "${terminfo[knp]}" down-line-or-history
+# create a zkbd compatible hash;
+# to add other keys to this hash, see: man 5 terminfo
+typeset -g -A key
 
-# Start typing + [Up-Arrow] - fuzzy find history forward
-autoload -U up-line-or-beginning-search
-zle -N up-line-or-beginning-search
-bindkey -M emacs "${terminfo[kcuu1]}" up-line-or-beginning-search
-# Start typing + [Down-Arrow] - fuzzy find history backward
-bindkey -M emacs "${terminfo[kcud1]}" down-line-or-beginning-search
 
-# [Home] - Go to beginning of line
-bindkey -M emacs "${terminfo[khome]}" beginning-of-line
-# [End] - Go to end of line
-bindkey -M emacs "${terminfo[kend]}"  end-of-line
+key[Home]="${terminfo[khome]}"
+key[End]="${terminfo[kend]}"
+key[Insert]="${terminfo[kich1]}"
+key[Backspace]="${terminfo[kbs]}"
+key[Delete]="${terminfo[kdch1]}"
+key[Up]="${terminfo[kcuu1]}"
+key[Down]="${terminfo[kcud1]}"
+key[Left]="${terminfo[kcub1]}"
+key[Right]="${terminfo[kcuf1]}"
+key[PageUp]="${terminfo[kpp]}"
+key[PageDown]="${terminfo[knp]}"
+key[Shift-Tab]="${terminfo[kcbt]}"
+# https://man.archlinux.org/man/user_caps.5
+key[Control-Left]="${terminfo[kLFT5]}"
+key[Control-Right]="${terminfo[kRIT5]}"
+key[Control-Delete]="${terminfo[kDC5]}"
+
+
+[[ -n "${key[Home]}"      ]] && bindkey -- "${key[Home]}"       beginning-of-line
+[[ -n "${key[End]}"       ]] && bindkey -- "${key[End]}"        end-of-line
+[[ -n "${key[Insert]}"    ]] && bindkey -- "${key[Insert]}"     overwrite-mode
+[[ -n "${key[Backspace]}" ]] && bindkey -- "${key[Backspace]}"  backward-delete-char
+[[ -n "${key[Delete]}"    ]] && bindkey -- "${key[Delete]}"     delete-char
+
+[[ -n "${key[Left]}"      ]] && bindkey -- "${key[Left]}"       backward-char
+[[ -n "${key[Right]}"     ]] && bindkey -- "${key[Right]}"      forward-char
+
+[[ -n "${key[PageUp]}"    ]] && bindkey -- "${key[PageUp]}"     up-line-or-history
+[[ -n "${key[PageDown]}"  ]] && bindkey -- "${key[PageDown]}"   down-line-or-history
 
 # [Shift-Tab] - move through the completion menu backwards
-bindkey -M emacs "${terminfo[kcbt]}" reverse-menu-complete
-# [Delete] - delete forward
-bindkey -M emacs "${terminfo[kdch1]}" delete-char
+[[ -n "${key[Shift-Tab]}" ]] && bindkey -- "${key[Shift-Tab]}"  reverse-menu-complete
 
-# [Ctrl-Delete] - delete whole forward-word
-bindkey -M emacs '^[[3;5~' kill-word
-# [Ctrl-RightArrow] - move forward one word
-bindkey -M emacs '^[[1;5C' forward-word
-# [Ctrl-LeftArrow] - move backward one word
-bindkey -M emacs '^[[1;5D' backward-word
+# Start typing + [Up-Arrow] - fuzzy find history forward
+autoload -U up-line-or-beginning-search down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+
+[[ -n "${key[Up]}"        ]] && bindkey -- "${key[Up]}"         up-line-or-beginning-search
+[[ -n "${key[Down]}"      ]] && bindkey -- "${key[Down]}"       down-line-or-beginning-search
+
+[[ -n "${key[Control-Delete]}"  ]] && bindkey -- "${key[Control-Delete]}"  kill-word
+
+[[ -n "${key[Control-Left]}"  ]] && bindkey -- "${key[Control-Left]}"  backward-word
+[[ -n "${key[Control-Right]}" ]] && bindkey -- "${key[Control-Right]}" forward-word
 
 # Edit the current command line in $EDITOR
 autoload -U edit-command-line
@@ -40,3 +62,13 @@ bindkey '\C-x\C-e' edit-command-line
 
 # file rename magick
 bindkey "^[m" copy-prev-shell-word
+
+# Finally, make sure the terminal is in application mode, when zle is
+# active. Only then are the values from $terminfo valid.
+if (( ${+terminfo[smkx]} && ${+terminfo[rmkx]} )); then
+	autoload -Uz add-zle-hook-widget
+	function zle_application_mode_start { echoti smkx }
+	function zle_application_mode_stop { echoti rmkx }
+	add-zle-hook-widget -Uz zle-line-init zle_application_mode_start
+	add-zle-hook-widget -Uz zle-line-finish zle_application_mode_stop
+fi
